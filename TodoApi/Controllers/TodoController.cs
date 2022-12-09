565d9f53670fcf2fff9885601499d105d3286fc5
M Hammad Raza <92687464+ham42@users.noreply.github.com>
2022-12-09 15:04:02 +0500

    Add files via upload

---
+++ TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.InteropServices;
+using TodoApi.Data;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoDbContext _db;
+        public TodoController(TodoDbContext context)
+        {
+            _db = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Todo>> Get()
+        {
+            return await _db.Todos.ToListAsync();
+        }
+
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(Todo), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var todo = await _db.Todos.FindAsync(id);
+            return todo == null ? NotFound() : Ok(todo);
+        }
+
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> Create(Todo todo)
+        {
+            await _db.Todos.AddAsync(todo);
+            await _db.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetById), new { id = todo.Id }, todo);
+        }
+
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Update(int id, Todo todo)
+        {
+            if (id != todo.Id) return BadRequest();
+
+            _db.Entry(todo).State = EntityState.Modified;
+            await _db.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var todo = await _db.Todos.FindAsync(id);
+            if(todo == null) return NotFound();
+
+            _db.Entry(todo).State = EntityState.Deleted;
+            await _db.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ TodoApi/Models/Todo.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoApi.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public GetPriority Priority { get; set; }
+
+        public GetStatus Status { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime? Completed { get; set; }
+
+
+        public enum GetPriority
+        {
+            Low, medium, High
+        }
+
+        public enum GetStatus
+        {
+            Pending, Completed, Uncompleted
+        }
+    }
+}
